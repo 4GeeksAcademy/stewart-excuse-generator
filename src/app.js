bc5c39060a90b0bb7d3c302e291fb8912c452675
stewybenz95 <113649226+stewybenz95@users.noreply.github.com>
2023-08-25 21:45:18 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+};
+let generateExcuse = () => {
+  let pronouns = ["a", "the", "my", "that"];
+  let subject = ["professional pickleball player", "dog", "doll"];
+  let action = ["ate", "shredded", "stole", "airfried"];
+  let possession = ["tv", "bike", "beard"];
+  let where = ["home", "mars", "shire"];
+
+  let proIndex = Math.floor(Math.random() * pronouns.length);
+  let subIndex = Math.floor(Math.random() * subject.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let possIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronouns[proIndex] +
+    " " +
+    subject[subIndex] +
+    " " +
+    action[actIndex] +
+    " " +
+    possession[possIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
